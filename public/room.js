e5bc7db44169e9fd8f204a3801561883915151b3
Lee Warrick <leewarrick@gmail.com>
2024-01-05 00:53:29 -0500

    fix protocol

--- public/room.js
+++ public/room.js
@@ -1,4 +1,5 @@
-const ws = new WebSocket("ws://" + location.host);
+const protocol = location.protocol === "https:" ? "wss://" : "ws://"
+const ws = new WebSocket(protocol + location.host);
 
 ws.onmessage = e => {
   const {type, data} = JSON.parse(e.data)
